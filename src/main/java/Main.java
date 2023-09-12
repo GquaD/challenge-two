9ba7c9fa2a8950eff98896afdd5b0b6d764c6a45
Behzod Tairov <tbekpro@gmail.com>
2023-09-12 11:22:43 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //in this program we are going to parse json objects
+    //and check whether given string is valid or invalid json object
+    public static void main(String[] args) {
+
+    }
+}
